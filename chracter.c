6b4cf4eb897606e8f86251c8b23c7f00565d9c78
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-03-07 10:18:25 +0530

    Create chracter.c

---
+++ chracter.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char str[100];
+int i=0,l=0,f=1;
+clrscr():
+printf("string")
+gets(str)
+for(i=0;str[i]!='/o';i++)
+{
+l=l+1
+}
+printf("no of character",f)
+getch();
+}
